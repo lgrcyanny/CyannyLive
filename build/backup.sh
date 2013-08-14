3d8988de3b4f0604a59fe9a37f6d5ee937aac900
lgrcyanny <lgrcyanny@gmail.com>
2013-08-14 23:41:25 +0800

    Update local links to production links in db.sql

--- build/backup.sh
+++ build/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export DATABASE_DIR=/Users/lgrcyanny/Sites/myblog/wp-content/backup-db
 cd $DATABASE_DIR
-mysqldump -u root -proot cyannyblog > db.sql
+mysqldump -u root -proot cyannyblog > temp-db.sql
+sed 's%http://cyanny/myblog%http://www.cyanny.com%g' temp-db.sql > db.sql
 tar -czvf db.tar.gz db.sql
 rm db.sql
+rm temp-db.sql
