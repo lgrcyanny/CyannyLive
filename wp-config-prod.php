3bbcc56a79bf6f63996a10958e73a9cf2c405f4c
lgrcyanny <lgrcyanny@gmail.com>
2013-08-24 22:13:00 +0800

    New Post fot set up VPS and deploy WP Blog

--- wp-config-prod.php
+++ wp-config-prod.php
@@ -17,8 +17,6 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 // //Added by WP-Cache Manager
-define('WP_CACHE', true); //Added by WP-Cache Manager
-define( 'WPCACHEHOME', '/var/www/html/cyannyblog/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
 define('DB_NAME', 'cyannyblog');
 
 /** MySQL database username */
