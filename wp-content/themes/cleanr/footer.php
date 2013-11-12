c870d8ccb892e4c15ade68145454cb8eacc5e47c
lgrcyanny <lgrcyanny@gmail.com>
2013-11-12 18:20:23 +0800

    Fix rss

--- wp-content/themes/cleanr/footer.php
+++ wp-content/themes/cleanr/footer.php
@@ -2,7 +2,7 @@
 <footer>
 <div id="footer" class="grid_16">
 	<p>
-		&copy; <?php echo date('Y'); ?>. <?php bloginfo('name'); ?> <a target="blank" href="https://github.com/lgrcyanny" title="Cyanny GitHub">GitHub</a>
+		&copy; <?php echo date('Y'); ?>. <?php bloginfo('name'); ?> | <a target="blank" href="https://github.com/lgrcyanny" title="Cyanny GitHub">GitHub</a> | <a target="blank" href="http://www.cyanny.com/feed" title="Cyanny Live RSS">RSS Feed</a>
 	</p>
 </div>
 </footer>
