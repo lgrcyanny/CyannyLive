e90f1269000e918e47b07836011a82e91de2af60
lgrcyanny <lgrcyanny@gmail.com>
2014-04-04 19:04:30 +0800

    Update footer

--- wp-content/themes/flat/footer.php
+++ wp-content/themes/flat/footer.php
@@ -1,8 +1,8 @@
 				<?php if ( apply_filters( 'show_flat_credits', true ) ) { ?>
 					<div class="site-info">
-						<?php do_action( 'flat_credits' ); ?>
-						<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flat' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flat' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flat' ), 'WordPress' ); ?></a>.
-						<?php printf( __( 'Theme: %1$s by %2$s.', 'flat' ), 'Flat', '<a href="'.esc_url( 'http://www.yoarts.com/' ).'" title="'.esc_attr('Webmaster Tutorials & Resources').'">YoArts</a>' ); ?>
+						<p>
+							&copy; <?php echo date('Y'); ?>. <?php bloginfo('name'); ?> | <a target="blank" href="https://github.com/lgrcyanny" title="Cyanny GitHub">GitHub</a> | <a target="blank" href="http://www.cyanny.com/feed" title="Cyanny Live RSS">RSS Feed</a>
+						</p>
 					</div><!-- .site-info -->
 				<?php } ?>
 			</div>
