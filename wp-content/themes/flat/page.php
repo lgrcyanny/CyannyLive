1e54ef409ab072aaf1e69b8372c3ebf308f16dda
lgrcyanny <lgrcyanny@gmail.com>
2014-05-12 14:44:42 +0800

    add page for jianyinjiatu

--- wp-content/themes/flat/page.php
+++ wp-content/themes/flat/page.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
-
+			<style type="text/css">
+				/**
+				 * For Jianyinjietu
+				 */
+				div#secondary {
+					display: none;
+				}
+				div#primary #content{
+					margin-left: 210px;
+					width: 850px;
+				}
+			</style>
 			<div id="content" class="site-content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -13,11 +24,11 @@
 						<?php the_content( __( 'Continue reading <span class="meta-nav">...</span>', 'flat' ) ); ?>
 
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'flat' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-					
+
 					</div>
 				</article>
 
-				<?php comments_template(); ?>
+				<?php //comments_template(); ?>
 
 			<?php endwhile; ?>
 
