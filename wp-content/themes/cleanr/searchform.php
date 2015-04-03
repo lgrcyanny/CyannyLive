8a7dd7376318a663630af6f3d8737998e13f5356
lgrcyanny <lgrcyanny@gmail.com>
2015-04-03 22:43:33 +0800

    new posts for find median algrithm

--- wp-content/themes/cleanr/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="field" name="s" id="s" placeholder="Search" />
-	<input type="image" src="<?php echo get_template_directory_uri(); ?>/images/search_magnifier.gif" id="searchbtn" value="Search" />
-</form>
